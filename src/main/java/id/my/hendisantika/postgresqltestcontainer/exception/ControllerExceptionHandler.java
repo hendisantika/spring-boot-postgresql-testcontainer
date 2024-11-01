70ac99e7324cc5dd7d6d00fff2269a5cd952e53d
hendisantika <hendisantika@yahoo.co.id>
2024-11-02 06:35:42 +0700

    Add onNoSuchElement in ControllerExceptionHandler🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/postgresqltestcontainer/exception/ControllerExceptionHandler.java
+++ src/main/java/id/my/hendisantika/postgresqltestcontainer/exception/ControllerExceptionHandler.java
@@ -1,6 +1,16 @@
 package id.my.hendisantika.postgresqltestcontainer.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.NoSuchElementException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,4 +30,15 @@ public class ControllerExceptionHandler {
 //	public ErrorResponse noSuchElement(NoSuchElementException e) {
 //		return ErrorResponse.builder().message(e.getMessage()).errorCode(404).status(HttpStatus.NOT_FOUND).build();
 //	}
+@ExceptionHandler(value = NoSuchElementException.class)
+public ProblemDetail onNoSuchElement(NoSuchElementException e) throws URISyntaxException {
+    ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
+    HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
+            .getRequest();
+
+    problemDetail.setInstance(new URI(request.getRequestURI()));
+    problemDetail.setType(URI.create("http://api.employees.com/errors/not-found"));
+    problemDetail.setTitle("Element Not Found");
+    return problemDetail;
+}
 }
