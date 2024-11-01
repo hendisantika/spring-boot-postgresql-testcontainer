6116336d0ceef7da931472453353a36cebd4bf67
hendisantika <hendisantika@yahoo.co.id>
2024-11-02 06:37:54 +0700

    Add UserInfoDTO🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/postgresqltestcontainer/response/UserInfoDTO.java
@@ -0,0 +1,27 @@
+package id.my.hendisantika.postgresqltestcontainer.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-postgresql-testcontainer
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 02/11/24
+ * Time: 06.37
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserInfoDTO {
+    private Integer id;
+    private String name;
+    private String email;
+    private String roles;
+}
