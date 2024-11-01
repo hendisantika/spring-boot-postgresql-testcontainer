f1cab9d7ad5dd11ffa7a3c7309b5442947fc2a95
hendisantika <hendisantika@yahoo.co.id>
2024-11-02 06:33:44 +0700

    Add UserInfo Entity🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/postgresqltestcontainer/entity/UserInfo.java
@@ -0,0 +1,43 @@
+package id.my.hendisantika.postgresqltestcontainer.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-postgresql-testcontainer
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 02/11/24
+ * Time: 06.33
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user_info")
+public class UserInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "roles")
+    private String roles;
+}
