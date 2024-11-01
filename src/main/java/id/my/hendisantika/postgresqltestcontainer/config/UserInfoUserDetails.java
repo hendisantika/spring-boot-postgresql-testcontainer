063f08505cd1005e97443235d731d5574045d2a3
hendisantika <hendisantika@yahoo.co.id>
2024-11-02 06:41:03 +0700

    Add UserInfoUserDetails🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/postgresqltestcontainer/config/UserInfoUserDetails.java
+++ src/main/java/id/my/hendisantika/postgresqltestcontainer/config/UserInfoUserDetails.java
@@ -1,9 +1,14 @@
 package id.my.hendisantika.postgresqltestcontainer.config;
 
+import id.my.hendisantika.postgresqltestcontainer.entity.UserInfo;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,8 +27,49 @@ public class UserInfoUserDetails implements UserDetails {
      */
     private static final long serialVersionUID = 1L;
 
-    private String username;
-    private String password;
-    private List<GrantedAuthority> authorities;
+    private final String username;
+    private final String password;
+    private final List<GrantedAuthority> authorities;
 
+    public UserInfoUserDetails(UserInfo userInfo) {
+        this.username = userInfo.getEmail();
+        this.password = userInfo.getPassword();
+        this.authorities = Arrays.stream(userInfo.getRoles().split(",")).map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
