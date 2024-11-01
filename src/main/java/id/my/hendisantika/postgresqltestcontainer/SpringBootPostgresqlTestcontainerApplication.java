92f3d2c3b090ecf017e4435153c725c9043465de
hendisantika <hendisantika@yahoo.co.id>
2024-11-02 06:27:28 +0700

    Create Spring Boot App🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/postgresqltestcontainer/SpringBootPostgresqlTestcontainerApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.postgresqltestcontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPostgresqlTestcontainerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPostgresqlTestcontainerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/postgresqltestcontainer/SpringBootPostgresqlTestcontainerApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.postgresqltestcontainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPostgresqlTestcontainerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
