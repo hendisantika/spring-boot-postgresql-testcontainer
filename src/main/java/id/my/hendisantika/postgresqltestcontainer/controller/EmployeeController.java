fafdf048ead4635340a6c51fe1aba0039a52c046
hendisantika <hendisantika@yahoo.co.id>
2024-11-02 06:50:11 +0700

    Add update in EmployeeController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/postgresqltestcontainer/controller/EmployeeController.java
+++ src/main/java/id/my/hendisantika/postgresqltestcontainer/controller/EmployeeController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -39,31 +40,37 @@ public class EmployeeController {
     @ResponseStatus(HttpStatus.OK)
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public List<EmployeeDTO> employees() {
-        return empoyeeService.employees();
+        return employeeService.employees();
     }
 
     @GetMapping(value = "/employees/{id}")
     @ResponseStatus(HttpStatus.OK)
     @PreAuthorize("hasAuthority('ROLE_USER')")
     public EmployeeDTO employee(@PathVariable(value = "id") Integer empId) {
-        return empoyeeService.employee(empId);
+        return employeeService.employee(empId);
     }
 
     @PostMapping(value = "/employees")
     @ResponseStatus(HttpStatus.CREATED)
     public EmployeeDTO save(@RequestBody EmployeeRequest emp) {
-        return empoyeeService.save(emp);
+        return employeeService.save(emp);
     }
 
     @DeleteMapping(value = "/employees")
     @ResponseStatus(HttpStatus.OK)
     public String deleteAll() {
-        return empoyeeService.deleteAll();
+        return employeeService.deleteAll();
     }
 
     @DeleteMapping(value = "/employees/{id}")
     @ResponseStatus(HttpStatus.OK)
     public String delete(@PathVariable(value = "id") Integer empId) {
-        return empoyeeService.delete(empId);
+        return employeeService.delete(empId);
+    }
+
+    @PutMapping(value = "/employees")
+    @ResponseStatus(HttpStatus.OK)
+    public EmployeeDTO update(@RequestBody EmployeeRequest emp) {
+        return employeeService.update(emp);
     }
 }
