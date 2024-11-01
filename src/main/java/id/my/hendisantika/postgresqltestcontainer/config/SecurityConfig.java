ff47042e291ff6fdecc98bbf98820e432543a775
hendisantika <hendisantika@yahoo.co.id>
2024-11-02 06:38:37 +0700

    Add userDetailsService in SecurityConfig🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/postgresqltestcontainer/config/SecurityConfig.java
+++ src/main/java/id/my/hendisantika/postgresqltestcontainer/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package id.my.hendisantika.postgresqltestcontainer.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,5 +20,16 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+    // authentication
+    @Bean
+//	public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+    public UserDetailsService userDetailsService() {
+//		UserDetails admin = User.withUsername("admin").password(encoder.encode("password")).roles("ADMIN").build();
+//		UserDetails user = User.withUsername("user").password(encoder.encode("pwd")).roles("USER").build();
+//		return new InMemoryUserDetailsManager(admin, user);
+
+        return new UserInfoService();
+
+    }
 
 }
