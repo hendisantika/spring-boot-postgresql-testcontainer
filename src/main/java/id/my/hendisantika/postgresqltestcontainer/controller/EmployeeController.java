facfebf05d34257958fe0ca73228cee4758a8770
hendisantika <hendisantika@yahoo.co.id>
2024-11-02 06:49:35 +0700

    Add deleteAll in EmployeeController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/postgresqltestcontainer/controller/EmployeeController.java
+++ src/main/java/id/my/hendisantika/postgresqltestcontainer/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -52,6 +53,12 @@ public class EmployeeController {
     @ResponseStatus(HttpStatus.CREATED)
     public EmployeeDTO save(@RequestBody EmployeeRequest emp) {
         return empoyeeService.save(emp);
+    }
+
+    @DeleteMapping(value = "/employees")
+    @ResponseStatus(HttpStatus.OK)
+    public String deleteAll() {
+        return empoyeeService.deleteAll();
     }
 
 }
