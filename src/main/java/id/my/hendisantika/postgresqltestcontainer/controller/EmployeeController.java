6a6425d176ed901575272b1990fa4a191d6847f8
hendisantika <hendisantika@yahoo.co.id>
2024-11-02 06:49:13 +0700

    Add employee in EmployeeController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/postgresqltestcontainer/controller/EmployeeController.java
+++ src/main/java/id/my/hendisantika/postgresqltestcontainer/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,5 +36,12 @@ public class EmployeeController {
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public List<EmployeeDTO> employees() {
         return empoyeeService.employees();
+    }
+
+    @GetMapping(value = "/employees/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public EmployeeDTO employee(@PathVariable(value = "id") Integer empId) {
+        return empoyeeService.employee(empId);
     }
 }
