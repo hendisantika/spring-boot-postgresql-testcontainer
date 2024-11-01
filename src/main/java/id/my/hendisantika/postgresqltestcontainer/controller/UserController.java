841beb62d6fab13c8281d8a2935abf7a617e919f
hendisantika <hendisantika@yahoo.co.id>
2024-11-02 06:51:12 +0700

    Add users in UserController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/postgresqltestcontainer/controller/UserController.java
+++ src/main/java/id/my/hendisantika/postgresqltestcontainer/controller/UserController.java
@@ -6,11 +6,14 @@ import id.my.hendisantika.postgresqltestcontainer.service.UserInfoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -33,5 +36,11 @@ public class UserController {
     @ResponseStatus(HttpStatus.CREATED)
     public UserInfoDTO addUser(@RequestBody UserInfoRequest userInfo) {
         return userInfoService.addUser(userInfo);
+    }
+
+    @GetMapping(value = "/user")
+    @ResponseStatus(HttpStatus.OK)
+    public List<UserInfoDTO> users() {
+        return userInfoService.users();
     }
 }
