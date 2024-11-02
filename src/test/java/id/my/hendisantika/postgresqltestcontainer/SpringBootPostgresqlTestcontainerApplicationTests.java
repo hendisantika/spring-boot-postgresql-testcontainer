446d7bde2bce4ccea2df88e2d23589009e4925be
hendisantika <hendisantika@yahoo.co.id>
2024-11-02 07:03:38 +0700

    Add static data🫸🌀✏️📗🐧🐳⬆

--- src/test/java/id/my/hendisantika/postgresqltestcontainer/SpringBootPostgresqlTestcontainerApplicationTests.java
+++ src/test/java/id/my/hendisantika/postgresqltestcontainer/SpringBootPostgresqlTestcontainerApplicationTests.java
@@ -37,11 +37,39 @@ class SpringBootPostgresqlTestcontainerApplicationTests {
 
     @Autowired
     private MockMvc mockMvc;
+
     @Autowired
     private EmployeeRepository employeeRepository;
+
     @Autowired
     private UserInfoRepository userInfoRepository;
+
     @Autowired
     private ObjectMapper objectMapper;
+
+    static {
+        EmployeeRequest emp1 = EmployeeRequest.builder().name("test emp1").address("address1").build();
+        EmployeeRequest emp2 = EmployeeRequest.builder().name("test emp2").address("address2").build();
+        EmployeeRequest emp3 = EmployeeRequest.builder().name("test emp3").address("address3").build();
+        EmployeeRequest emp4 = EmployeeRequest.builder().name("test emp4").address("address4").build();
+        EmployeeRequest emp5 = EmployeeRequest.builder().name("test emp5").address("address5").build();
+
+        employees.add(emp1);
+        employees.add(emp2);
+        employees.add(emp3);
+        employees.add(emp4);
+        employees.add(emp5);
+
+        UserInfoRequest admin = UserInfoRequest.builder().name("admin").email("admin@gmail.com").password("password")
+                .roles("ROLE_ADMIN").build();
+        UserInfoRequest user = UserInfoRequest.builder().name("user").email("user@gmail.com").password("password")
+                .roles("ROLE_USER").build();
+        UserInfoRequest adminUser = UserInfoRequest.builder().name("adminuser").email("adminuser@gmail.com")
+                .password("password").roles("ROLE_ADMIN,ROLE_USER").build();
+
+        users.add(admin);
+        users.add(user);
+        users.add(adminUser);
+    }
 
 }
